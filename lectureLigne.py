14f427eb096b18143c809823ff44a4588b6b04c1
Argelll <81804462+Argelll@users.noreply.github.com>
2021-04-06 16:13:05 +0200

    Update lectureLigne.py

--- lectureLigne.py
+++ lectureLigne.py
@@ -9,7 +9,7 @@ nomFichier = sys.argv[0]
 
 fichierEntier = Files(nomFichier)
 listeGene=[]
-toutesLignes = fichier_entier.split("\n")
+toutesLignes = fichierEntier.split("\n")
 for ligne in toutesLignes :
   position=ligne.split("\t")
   unGene= Gene(position[0],position[1],position[2],position[3],position[4],position[5],position[6],position[7])
