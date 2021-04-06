6aacad443a20e4b8fb0d7b794cf2b9a7ab01444b
Argelll <81804462+Argelll@users.noreply.github.com>
2021-04-06 17:04:22 +0200

    Update lectureLigne.py

--- lectureLigne.py
+++ lectureLigne.py
@@ -15,5 +15,3 @@ for ligne in toutesLignes :
   try :
     unGene= Gene(position[0],position[1],position[2],position[3],position[4],position[5],position[6],position[7])
     listeGene.append(unGene)
-  except:
-  
