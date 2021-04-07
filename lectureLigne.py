65bc66aa4afa830aec81899941bd799a00036106
Argelll <81804462+Argelll@users.noreply.github.com>
2021-04-07 11:32:49 +0200

    Update lectureLigne.py

--- lectureLigne.py
+++ lectureLigne.py
@@ -13,9 +13,11 @@ listeGene=[]
 toutesLignes=fichier.Lecture()
 for ligne in toutesLignes :
   position=ligne.split("\t")
+  for parties in position
+    print (parties+";")
   try :
-    unGene= Gene(position[0],position[1],position[2],position[3],position[4],position[5],position[6],position[7])
-    listeGene.append(unGene)
+    #unGene= Gene(position[0],position[1],position[2],position[3],position[4],position[5],position[6],position[7])
+    #listeGene.append(unGene)
   except :
     print ("pas cette ligne")
     
