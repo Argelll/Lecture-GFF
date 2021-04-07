59f2362847d7aea72f0ea5307e3a43bdd73f89e6
Argelll <81804462+Argelll@users.noreply.github.com>
2021-04-07 11:33:36 +0200

    Update lectureLigne.py

--- lectureLigne.py
+++ lectureLigne.py
@@ -13,7 +13,7 @@ listeGene=[]
 toutesLignes=fichier.Lecture()
 for ligne in toutesLignes :
   position=ligne.split("\t")
-  for parties in position
+  for parties in position:
     print (parties+";")
   try :
     #unGene= Gene(position[0],position[1],position[2],position[3],position[4],position[5],position[6],position[7])
