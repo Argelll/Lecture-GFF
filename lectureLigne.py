8564d6347bd690353371a5ecec0d80e24a27d4f7
Argelll <81804462+Argelll@users.noreply.github.com>
2021-04-07 11:23:47 +0200

    Update lectureLigne.py

--- lectureLigne.py
+++ lectureLigne.py
@@ -6,6 +6,7 @@ from LectureFichier import *
 from ClassGene import *
 print ("on y passe")
 nomFichier = sys.argv[0]
+print (nomFichier)
 
 fichier = Files(nomFichier)
 listeGene=[]
@@ -14,7 +15,6 @@ for ligne in toutesLignes :
   position=ligne.split("\t")
   try :
     unGene= Gene(position[0],position[1],position[2],position[3],position[4],position[5],position[6],position[7])
-    
     listeGene.append(unGene)
   except :
     print ("pas cette ligne")
